157da4d32734f38a924a4bfef585692f8d1c1f49
Peter Kulko <93188219+PKulkoRaccoonGang@users.noreply.github.com>
2025-11-04 16:56:49 +0200

    feat: [FC-86] Added Course About sidebar (#38)
    * feat: [FC-86] Added Course About sidebar
    
    refactor: some refactoring
    
    refactor: some refactoring
    
    test: added tests
    
    refactor: added plugin slots
    
    refactor: changed page layout
    
    test: added some tests
    
    refactor: some refactoring
    
    refactor: corrected formatDate function
    
    * refactor: after review
    
    * refactor: after review

--- src/course-about/CourseAboutPage.tsx
+++ src/course-about/CourseAboutPage.tsx
@@ -1,5 +1,7 @@
 import { useLocation } from 'react-router';
-import { Container, Layout, Alert } from '@openedx/paragon';
+import {
+  Container, Layout, Alert, useMediaQuery, breakpoints, Stack,
+} from '@openedx/paragon';
 import { ErrorPage } from '@edx/frontend-platform/react';
 import { getConfig } from '@edx/frontend-platform';
 import { useIntl } from '@edx/frontend-platform/i18n';
@@ -8,16 +10,15 @@ import { Loading } from '@src/generic';
 import CourseAboutIntroSlot from '@src/plugin-slots/CourseAboutIntroSlot';
 import CourseAboutCourseMediaSlot from '@src/plugin-slots/CourseAboutCourseMediaSlot';
 import { useCourseAboutData } from './data/hooks';
+import CourseSidebar from './course-sidebar/CourseSidebar';
+import { CourseOverview } from './course-overview';
 import messages from './messages';
-
-export const GRID_LAYOUT = {
-  xs: [{ span: 12 }, { span: 'auto' }],
-  xl: [{ span: 9 }, { span: 3 }],
-};
+import { GRID_LAYOUT } from './layout';
 
 const CourseAboutPage = () => {
   const intl = useIntl();
   const courseId = useLocation().pathname.split('/')[2];
+  const isSmallScreen = useMediaQuery({ maxWidth: breakpoints.large.maxWidth });
   const {
     data: courseAboutData,
     isLoading,
@@ -43,13 +44,38 @@ const CourseAboutPage = () => {
   }
 
   return (
-    <Container fluid={false} size="xl" className="course-about-intro-wrapper py-5.5">
+    <Container fluid={false} size="xl" className="py-5.5">
       <Layout {...GRID_LAYOUT}>
         <Layout.Element>
-          <CourseAboutIntroSlot courseAboutData={courseAboutData} />
+          {isSmallScreen ? (
+            <Stack gap={4}>
+              <Layout.Element className="course-media-wrapper text-center">
+                <CourseAboutCourseMediaSlot courseAboutData={courseAboutData} />
+              </Layout.Element>
+              <CourseAboutIntroSlot courseAboutData={courseAboutData} />
+              <CourseOverview />
+              <aside>
+                <CourseSidebar courseAboutData={courseAboutData} />
+              </aside>
+            </Stack>
+          ) : (
+            <Stack gap={4}>
+              <CourseAboutIntroSlot courseAboutData={courseAboutData} />
+              <CourseOverview />
+            </Stack>
+          )}
         </Layout.Element>
-        <Layout.Element className="course-media-wrapper">
-          <CourseAboutCourseMediaSlot courseAboutData={courseAboutData} />
+        <Layout.Element>
+          {!isSmallScreen && (
+            <Stack gap={4}>
+              <Layout.Element className="course-media-wrapper">
+                <CourseAboutCourseMediaSlot courseAboutData={courseAboutData} />
+              </Layout.Element>
+              <aside>
+                <CourseSidebar courseAboutData={courseAboutData} />
+              </aside>
+            </Stack>
+          )}
         </Layout.Element>
       </Layout>
     </Container>
